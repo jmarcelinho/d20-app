1b845e0be2aecbfb3af32998374ea031fa04f7eb
João Marcelo <joao.marcelo.junior@ccc.ufcg.edu.br>
2019-09-08 18:04:48 -0300

    Deploy heroku

---
+++ src/main/java/com/example/d20/D20Application.java
@@ -0,0 +1,13 @@
+package com.example.d20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D20Application.class, args);
+	}
+
+}
