8aa0f2f3150407a7f8f508d0b38ec5c2492b9b84
Matheus Oliveira <matheus7op@gmail.com>
2019-09-17 23:49:22 -0300

    Reverting unwanted deletes made automatically by Eclipse

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -1,5 +1,71 @@
 package com.example.controllers;
 
-public class UserController {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.User;
+import com.example.services.UserService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public List<User> getAll() {
+		return this.userService.getAllUsers();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUsuarioById(@PathVariable Integer id){
+		User user = this.userService.getUserById(id);
+		
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> add(@Valid @RequestBody User userBody){
+		User user = this.userService.addUser(userBody);
+		return ResponseEntity.ok(user);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable Integer id, @Valid @RequestBody User userBody){
+		User user = this.userService.updateUser(id, userBody);
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Integer id) {
+		boolean t = this.userService.delete(id);
+		
+		if (!t) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -1,5 +1,59 @@
 package com.example.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> getAllUsers() {
+		return this.userRepository.findAll();
+	}
+	
+	public User getUserById(Integer id) {
+		return this.userRepository.findById(id).get();
+	}
+	
+	public User addUser(User user) {
+		return this.userRepository.save(user);
+	}
+	
+	//fazer verificacao para quando usuario for nulo
+	public User updateUser(Integer id, User user) {
+		User newUser = this.getUserById(id);
+		
+		if(newUser != null) {
+			newUser.setCpf(user.getCpf());
+			newUser.setBankInfo(user.getBankInfo());
+			newUser.setCredCard(user.getCredCard());
+			newUser.setName(user.getName());
+			newUser.setTelephone(user.getTelephone());
+		}
+		
+		return newUser;
+	}
+	
+	public boolean delete(Integer id) {
+		User newUser = this.getUserById(id);
+		
+		if(newUser != null) {
+			this.userRepository.deleteById(id);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public List<User> getUserByNome(String nome){
+		return this.userRepository.findAllByNome(nome);
+	}
 
 }
