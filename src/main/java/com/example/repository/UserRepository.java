c9a53c990cacb5d05ca4381c098086fea6a7c546
Matheus Oliveira <matheus7op@gmail.com>
2019-09-17 23:47:23 -0300

    Adding models to Game and Ownership

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,14 +1,9 @@
 package com.example.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-	User findByCpf(String cpf);
-	List<User> findAllByNome(String nome);
+
 }
