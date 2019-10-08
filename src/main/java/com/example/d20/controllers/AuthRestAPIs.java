e1aa91886cad2acaf49c8feeb534f97932498fe4
AmandioF <amandio31123@gmail.com>
2019-10-08 03:37:00 -0300

    Adding Security

---
+++ src/main/java/com/example/d20/controllers/AuthRestAPIs.java
@@ -0,0 +1,113 @@
+package com.example.d20.controllers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.d20.message.request.LoginForm;
+import com.example.d20.message.request.SignUpForm;
+import com.example.d20.message.response.JwtResponse;
+import com.example.d20.model.Role;
+import com.example.d20.model.RoleName;
+import com.example.d20.model.Account;
+import com.example.d20.repository.RoleRepository;
+import com.example.d20.repository.AccountRepository;
+import com.example.d20.security.jwt.JwtProvider;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestAPIs {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateJwtToken(authentication);
+        return ResponseEntity.ok(new JwtResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody SignUpForm signUpRequest) {
+        if(accountRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity<String>("Fail -> Username is already taken!",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if(accountRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity<String>("Fail -> Email is already in use!",
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        Account user = new Account(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        strRoles.forEach(role -> {
+        	switch(role) {
+	    		case "admin":
+	    			Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	    			roles.add(adminRole);
+	    			
+	    			break;
+	    		case "pm":
+	            	Role pmRole = roleRepository.findByName(RoleName.ROLE_PM)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	            	roles.add(pmRole);
+	            	
+	    			break;
+	    		default:
+	        		Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+	                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+	        		roles.add(userRole);        			
+        	}
+        });
+        
+        user.setRoles(roles);
+        accountRepository.save(user);
+
+        return ResponseEntity.ok().body("User registered successfully!");
+    }
+}
---
+++ src/main/java/com/example/d20/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.example.d20.model;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
