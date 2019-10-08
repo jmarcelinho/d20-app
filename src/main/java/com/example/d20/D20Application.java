47e6757d1d6a10f0e7b35d893a0160cb7ef85774
AmandioF <amandio31123@gmail.com>
2019-10-08 06:07:46 -0300

    Update D20Application.java

--- src/main/java/com/example/d20/D20Application.java
+++ src/main/java/com/example/d20/D20Application.java
@@ -1,5 +1,6 @@
 package com.example.d20;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,8 @@ import com.example.d20.repository.RoleRepository;
 @SpringBootApplication
 public class D20Application implements CommandLineRunner {
 	
-	static RoleRepository roleRepository;
+	@Autowired
+	RoleRepository roleRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(D20Application.class, args);
