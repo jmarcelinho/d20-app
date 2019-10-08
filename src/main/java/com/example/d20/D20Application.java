97bf76f6e7d6a06e9ae935ab1a1ee8167952cf09
AmandioF <amandio31123@gmail.com>
2019-10-08 05:38:04 -0300

    adding roles

--- src/main/java/com/example/d20/D20Application.java
+++ src/main/java/com/example/d20/D20Application.java
@@ -3,11 +3,20 @@ package com.example.d20;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.d20.model.Role;
+import com.example.d20.model.RoleName;
+import com.example.d20.repository.RoleRepository;
+
 @SpringBootApplication
 public class D20Application {
-
+	
+	static RoleRepository roleRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(D20Application.class, args);
+		roleRepository.save(new Role(RoleName.ROLE_USER));
+		roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+		roleRepository.save(new Role(RoleName.ROLE_PM));
 	}
 
 }
