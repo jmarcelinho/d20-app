835c05f497e4d3dd55dd358680b9823681af8a32
AmandioF <amandio31123@gmail.com>
2019-10-09 07:02:02 -0300

    adding schema

--- src/main/java/com/example/d20/model/Role.java
+++ src/main/java/com/example/d20/model/Role.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import org.hibernate.annotations.NaturalId;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "roles", schema="d20-app")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/d20/model/User.java
+++ src/main/java/com/example/d20/model/User.java
@@ -19,7 +19,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.annotations.NaturalId;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_user", schema="d20-app")
 public class User{
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
