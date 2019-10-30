33cbf6814cf395962c1d062520617520f6dbf85e
AmandioF <amandio31123@gmail.com>
2019-10-29 21:57:59 -0300

    WiP adjusting

--- src/main/java/com/example/d20/repository/AccountRepository.java
+++ src/main/java/com/example/d20/repository/AccountRepository.java
@@ -10,6 +10,5 @@ import com.example.d20.model.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByEmail(String email);
-    Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
