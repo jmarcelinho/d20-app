242da21341c561050bd7214901fc1b20b2b9e523
AmandioF <amandio31123@gmail.com>
2019-11-07 23:13:35 -0300

    ADD AccountService

--- src/main/java/com/example/d20/repository/UserRepository.java
+++ src/main/java/com/example/d20/repository/UserRepository.java
@@ -12,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	User findByCpf(String cpf);
 	User findByEmail(String name);
 	List<User> findAllByFname(String name);
+	List<User> findAllByLname(String name);
 }
