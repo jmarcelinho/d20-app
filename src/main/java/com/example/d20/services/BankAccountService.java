7d6cf40b366764c3c075e34dd9a110a57fb6beff
Joao Marcelo <marcelinhofernandes2013@gmail.com>
2019-11-11 19:48:28 -0300

    adicionando classe service

--- src/main/java/com/example/d20/services/BankAccountService.java
+++ src/main/java/com/example/d20/services/BankAccountService.java
@@ -1,5 +1,15 @@
 package com.example.d20.services;
 
-public class BankAccountService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.d20.repository.BankAccountRepository;
+
+@Service
+public class BankAccountService {
+	
+	@Autowired
+	private BankAccountRepository bankRepository;
+	
+	
 }
